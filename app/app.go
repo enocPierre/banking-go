a3624c5e94428b464235a10df0396a9f414de84d
enocPierre <enoc.pierre@mercadolivre.com>
2022-01-30 15:03:42 -0300

    refactoring server

--- app/app.go
+++ app/app.go
@@ -4,11 +4,14 @@ package app
 import (
 	"log"
 	"net/http"
+	"github.com/gorrila/mux"
 )
 
 	func Start() {
 
-	mux := http.NewServeMux()
+	//mux := http.NewServeMux()
+	mux := mux.NewRouter()
+
 
 	//define routes
 	mux.HandleFunc("/greet", greet)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/golang/Bancking
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
