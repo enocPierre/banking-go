4c64f4b4909599e7ad2a67f706f6d6405fe440e8
enocPierre <enoc.pierre@mercadolivre.com>
2022-01-30 11:23:52 -0300

    xml  encoding

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"encoding/json"
+	//"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name	string `json:"full_name"`
-	City	string `json:"city"`
-	Zipcode string `json:"zip_code"`
+	Name	string `json:"full_name" xml:"name"`
+	City	string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zipcode"`
 }
 
 func main () {
@@ -33,6 +34,10 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 		{"Pierre", "Philadelphia", "234567"},
 	}
 
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(customers)
+	//w.Header().Add("Content-Type", "application/json")
+	w.Header().Add("Content-Type", "application/xml")
+
+	//json.NewEncoder(w).Encode(customers)
+	xml.NewEncoder(w).Encode(customers)
+
 }
