5fbcea3c4e445c197be3bca3572778c566ff45fb
enocPierre <enoc.pierre@mercadolivre.com>
2022-01-30 11:14:13 -0300

    json encoding

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-
+type Customer struct {
+	Name	string `json:"full_name"`
+	City	string `json:"city"`
+	Zipcode string `json:"zip_code"`
+}
 
 func main () {
-		//define routes
+
+	//define routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	// starting server
 	log.Fatal(http.ListenAndServe("localhost: 8000", nil))
@@ -18,4 +25,14 @@ func main () {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello word!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer {
+		{"Ashis", "New Oleans", "23409"},
+		{"Pierre", "Philadelphia", "234567"},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
